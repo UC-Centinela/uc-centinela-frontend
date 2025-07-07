92ad90601c2d51b46fad43070e221ade6ded6c1e
Astugol <diego.astudillo@uc.cl>
2025-07-07 18:46:07 -0400

    feat: add critical activities fetching and update redirection logic in SendPage
    - Introduced `getCriticalActivitiesByTaskId` function to fetch critical activities related to a task.
    - Updated redirection logic in the SendPage component to handle cases where no critical activities are found, improving navigation flow.

--- src/app/tasks/[task_id]/send/page.tsx
+++ src/app/tasks/[task_id]/send/page.tsx
@@ -1,6 +1,7 @@
 import { getTaskData, validateTaskAccess } from "@/services/task";
 import { notFound, redirect } from "next/navigation";
 import SendContent from "./SendContent";
+import { getCriticalActivitiesByTaskId } from "@/services/criticalActivities";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default async function SendPage({ params }: any) {
@@ -12,10 +13,13 @@ export default async function SendPage({ params }: any) {
   }
 
   const taskData = await getTaskData(task_id);
+  const criticalActivities = await getCriticalActivitiesByTaskId(task_id);
   if (taskData?.state === "REVIEWED") {
     redirect(`/tasks/${task_id}/approved`);
   } else if (taskData?.state === "IS_REJECTED") {
     redirect(`/tasks`);
+  } else if (criticalActivities.length === 0) {
+    redirect(`/tasks/${task_id}`);
   }
 
   return <SendContent taskId={task_id} />;
